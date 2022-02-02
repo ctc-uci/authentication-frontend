774444a497c01d750b52c45a4f7313c34b7fe89b
claudeisakeeb <claudeyan2001@gmail.com>
2022-02-01 18:25:05 -0800

    Rename imported Login/Logout

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import './App.css';
 
-import LoginTest from './components/Login';
-import LogoutTest from './components/Logout';
+import Login from './components/Login';
+import Logout from './components/Logout';
 
 function App() {
   return (
     <CookiesProvider>
       <Router>
         <Routes>
-          <Route exact path="/" element={<LoginTest />} />
-          <Route exact path="/logout" element={<LogoutTest />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/logout" element={<Logout />} />
         </Routes>
       </Router>
     </CookiesProvider>
